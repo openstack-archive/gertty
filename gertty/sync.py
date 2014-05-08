2f9b403c3d05c88f27b11329268c62aaad6e2669
Clark Boylan <clark.boylan@gmail.com>
2014-05-07 18:35:55 -0700

    Ignore closed changes more.
    Gertty previously synced any change it knew about and listed any change
    not explicitly marked as reviewed in the unreviewed list. Update this to
    ignore MERGED and ABANDONED changes when syncing and don't list these
    changes in the unreviewed list either. Changes that are not open have
    been reveiwed and closed and can be ignored by the user.
    
    Note this is not symmetric currently with listing reviewed changes and
    all changes. I think gertty should also learn to ignore and remove
    MERGED changes from its DB entirely at some point. But not quite sure
    how we want to do that. Might only be useful to do that after we add the
    ability to view specific changes.
    
    Change-Id: I951a687250b79eec7eca00f877cb1cb50e867c09

--- gertty/sync.py
+++ gertty/sync.py
@@ -30,6 +30,8 @@ except:
     pass
 import requests
 
+from gertty.db import closed_statuses
+
 HIGH_PRIORITY=0
 NORMAL_PRIORITY=1
 LOW_PRIORITY=2
@@ -119,8 +121,6 @@ class SyncSubscribedProjectsTask(Task):
                 sync.submitTask(SyncProjectTask(p.key, self.priority))
 
 class SyncProjectTask(Task):
-    _closed_statuses = ['MERGED', 'ABANDONED']
-
     def __init__(self, project_key, priority=NORMAL_PRIORITY):
         super(SyncProjectTask, self).__init__(priority)
         self.project_key = project_key
@@ -146,7 +146,8 @@ class SyncProjectTask(Task):
                 # For now, just sync open changes or changes already
                 # in the db optionally we could sync all changes ever
                 change = session.getChangeByID(c['id'])
-                if change or (c['status'] not in self._closed_statuses):
+                if ((change and (change.status not in self.closed_statuses)) or
+                    (c['status'] not in self.closed_statuses)):
                     sync.submitTask(SyncChangeTask(c['id'], self.priority))
                     self.log.debug("Change %s update %s" % (c['id'], c['updated']))
 
