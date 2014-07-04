e38d1502e647f49e978737768ce2fd716f9b674d
Robert Collins <rbtcollins@hp.com>
2014-07-04 19:13:27 +1200

    Create local refs to prevent pruning
    By default git will prune dangling commits more than 2 weeks old. All
    the unmerged versions of reviews pushed up more than 2 weeks ago count
    as such commits, so we're downloading all the history again and again
    each time git-gc --auto ends up being invoked (which is triggered
    amongst other things by loose object counts. This leads to random
    stalls in gertty when it has to redownload those objects - and this
    is obviously worse when offline.
    
    This commit adds refs to prevent this behaviour, but doesn't remove
    them at any point. We probably want to facilitate gc of
    merged/abandoned/ignored commits at some point. I'm not sure if that
    needs to happen in this commit or if it could be added later.
    
    Change-Id: I01c61fd65207fe0dc99208241eefd5d6432c2ad0

--- gertty/sync.py
+++ gertty/sync.py
@@ -255,7 +255,7 @@ class SyncChangeTask(Task):
                                            sync.app.config.password, url[1])
                     url = urlparse.urlunsplit(url)
                 if (not revision) or self.force_fetch:
-                    fetches[url].append(ref)
+                    fetches[url].append('+%(ref)s:%(ref)s' % dict(ref=ref))
                 if not revision:
                     revision = change.createRevision(remote_revision['_number'],
                                                      remote_revision['commit']['message'], remote_commit,
@@ -436,7 +436,7 @@ class FetchRefTask(Task):
             url = urlparse.urlunsplit(url)
         self.log.debug("git fetch %s %s" % (url, self.refs))
         repo = sync.app.getRepo(self.project_name)
-        refs = self.refs
+        refs = ['+%(ref)s:%(ref)s' % dict(ref=ref) for ref in self.refs]
         try:
             repo.fetch(url, refs)
         except Exception:
