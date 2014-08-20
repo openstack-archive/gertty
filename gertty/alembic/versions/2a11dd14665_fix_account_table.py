823ccfb3a3ea46483b6be7f5add923e78c946853
James E. Blair <jeblair@hp.com>
2014-08-20 13:12:02 -0700

    Fix account table indexes
    The migration that introduced the account table totally botched
    the indexes.
    
    Change-Id: I69fce9cb092ac0819df15887076784d8f754ac96

---
+++ gertty/alembic/versions/2a11dd14665_fix_account_table.py
@@ -0,0 +1,26 @@
+"""fix account table
+
+Revision ID: 2a11dd14665
+Revises: 4cc9c46f9d8b
+Create Date: 2014-08-20 13:07:25.079603
+
+"""
+
+# revision identifiers, used by Alembic.
+revision = '2a11dd14665'
+down_revision = '4cc9c46f9d8b'
+
+from alembic import op
+import sqlalchemy as sa
+
+
+def upgrade():
+    op.drop_index('ix_account_name')
+    op.drop_index('ix_account_username')
+    op.drop_index('ix_account_email')
+    op.create_index(op.f('ix_account_name'), 'account', ['name'])
+    op.create_index(op.f('ix_account_username'), 'account', ['username'])
+    op.create_index(op.f('ix_account_email'), 'account', ['email'])
+
+def downgrade():
+    pass
