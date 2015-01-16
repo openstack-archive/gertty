openstack-archive/gertty

commit 3e23dbad0f485fd8243d5a0ae985aaa86de43e51

Handle unicode emails in git commits
This should probably never happen, but it has anyway, so assume the
email field is a utf8 string.  This should avoid crashing in case
someone has a utf8 email, but may not be a complete solution to
the case that an email is in a non-ascii, non-utf8 charset.

Change-Id: I95ed91e5fdfe35b73550e1824e609a0b1d388b3a

gertty/gitrepo.py
@@ -83,12 +83,14 @@ class CommitContext(object):
             commit.authored_date, commit.author_tz_offset)
         commit_date = self.decorateGitTime(
             commit.committed_date, commit.committer_tz_offset)
-        return ["Parent: %s\n" % parentsha,
-                "Author: %s <%s>\n" % (author.name, author.email),
-                "AuthorDate: %s\n" % author_date,
-                "Commit: %s <%s>\n" % (committer.name, committer.email),
-                "CommitDate: %s\n" % commit_date,
-                "\n"] + commit.message.splitlines(True)
+        return [u"Parent: %s\n" % parentsha,
+                u"Author: %s <%s>\n" % (author.name,
+                                        unicode(author.email, 'utf8')),
+                u"AuthorDate: %s\n" % author_date,
+                u"Commit: %s <%s>\n" % (committer.name,
+                                        unicode(committer.email, 'utf')),
+                u"CommitDate: %s\n" % commit_date,
+                u"\n"] + commit.message.splitlines(True)
 
     def __init__(self, old, new):
         """Create a CommitContext.

commit 12edc3aaf996b9491f5504401ffcaf1d7ef47175

Only decode email if already encoded
* gertty/gitrepo.py(CommitContext.decorateMessage): A regression was
introduced by 3e23dba where some platforms already used unicode for
the author.email and committer.email values. In those circumstances
attempting to decode from UTF-8 fails spectacularly, so now we only
decode when we need to.

Change-Id: I267a4cb7ff35a8c864a7f3396abd5bf44ecd1ffc

gertty/gitrepo.py
@@ -83,12 +83,18 @@ class CommitContext(object):
             commit.authored_date, commit.author_tz_offset)
         commit_date = self.decorateGitTime(
             commit.committed_date, commit.committer_tz_offset)
+        if type(author.email) is unicode:
+            author_email = author.email
+        else:
+            author_email = unicode(author.email, 'utf8')
+        if type(committer.email) is unicode:
+            committer_email = committer.email
+        else:
+            committer_email = unicode(committer.email, 'utf8')
         return [u"Parent: %s\n" % parentsha,
-                u"Author: %s <%s>\n" % (author.name,
-                                        unicode(author.email, 'utf8')),
+                u"Author: %s <%s>\n" % (author.name, author_email),
                 u"AuthorDate: %s\n" % author_date,
-                u"Commit: %s <%s>\n" % (committer.name,
-                                        unicode(committer.email, 'utf')),
+                u"Commit: %s <%s>\n" % (committer.name, committer_email),
                 u"CommitDate: %s\n" % commit_date,
                 u"\n"] + commit.message.splitlines(True)
 