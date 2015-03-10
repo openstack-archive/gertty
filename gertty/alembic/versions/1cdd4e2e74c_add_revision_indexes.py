461a0eee45c568668264d852e5467cbe5202bdfc
James E. Blair <jeblair@hp.com>
2015-03-10 16:32:59 -0700

    Add indexes to revision table
    The commit and parent columns were not indexed, meaning that checking to see
    if a commit's parent needed to be synced incurred a full table scan.  Add
    indexes so it's faster.
    
    Change-Id: I093f8265a655a1fe2b7ba1be792b87e6d701fec1

---
+++ gertty/alembic/versions/1cdd4e2e74c_add_revision_indexes.py
@@ -0,0 +1,23 @@
+"""add revision indexes
+
+Revision ID: 1cdd4e2e74c
+Revises: 4a802b741d2f
+Create Date: 2015-03-10 16:17:41.330825
+
+"""
+
+# revision identifiers, used by Alembic.
+revision = '1cdd4e2e74c'
+down_revision = '4a802b741d2f'
+
+from alembic import op
+import sqlalchemy as sa
+
+
+def upgrade():
+    op.create_index(op.f('ix_revision_commit'), 'revision', ['commit'])
+    op.create_index(op.f('ix_revision_parent'), 'revision', ['parent'])
+
+
+def downgrade():
+    pass
