12ab41a67444e49687f44978110d09f3885bb2a6
Christoph Gysin <christoph.gysin@gmail.com>
2015-03-29 00:43:44 +0200

    Add support for auth-type=form
    I'm using a gerrit installation that uses neither 'basic' nor 'digest'
    authentication methods, but instead redirects users to a custom login
    form which sets a cookie to provide authentication.
    
    This commit adds a new auth-type 'form', that automates logging in
    through that form.
    
    Currently, the path to the form is hardcoded to '/login' and the names
    of the username and password fields to 'username' and 'password'
    respectively. If preferred, I can add configuration options for each of
    those too.
    
    Change-Id: I3fff38b1be3f17c753787c9eb62ede0ce60a3ece

--- gertty/sync.py
+++ gertty/sync.py
@@ -35,6 +35,7 @@ import requests
 import requests.utils
 
 import gertty.version
+from gertty.auth import FormAuth
 
 HIGH_PRIORITY=0
 NORMAL_PRIORITY=1
@@ -890,6 +891,8 @@ class Sync(object):
         self.session = requests.Session()
         if self.app.config.auth_type == 'basic':
             authclass = requests.auth.HTTPBasicAuth
+        elif self.app.config.auth_type == 'form':
+            authclass = FormAuth
         else:
             authclass = requests.auth.HTTPDigestAuth
         self.auth = authclass(
