e9d7a09d06f84fadc63ca9482729a7d116c9ac0b
John L. Villalovos <john.l.villalovos@intel.com>
2017-08-10 09:59:51 -0700

    Change usage of exit() to sys.exit()
    sys.exit() is the correct way to terminate.
    
    It is recommended to not use exit() in programs[1]:
        The site module (which is imported automatically during startup,
        except if the -S command-line option is given) adds several
        constants to the built-in namespace. They are useful for the
        interactive interpreter shell and should not be used in programs.
    
    [1] https://docs.python.org/3/library/constants.html#exit
    
    Change-Id: I4bfeba2f351c51a4da2e283ee44e10b76f863ff6

--- gertty/config.py
+++ gertty/config.py
@@ -137,7 +137,7 @@ class Config(object):
 
         if not os.path.exists(self.path):
             self.printSample()
-            exit(1)
+            sys.exit(1)
 
         self.config = yaml.load(open(self.path))
         schema = ConfigSchema().getSchema(self.config)
@@ -164,7 +164,7 @@ class Config(object):
                     "Error: Config file '{}' contains a password and does "
                     "not have permissions set to 0600.\n"
                     "Permissions are: {}".format(self.path, oct(mode)))
-                exit(1)
+                sys.exit(1)
         self.auth_type = server.get('auth-type', 'digest')
         self.verify_ssl = server.get('verify-ssl', True)
         if not self.verify_ssl:
